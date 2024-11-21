d5b62554de93b4a5df76ca713c6136758621ea9b
claauzuma <c.zumarraga94@hotmail.com>
2024-11-21 20:07:31 -0300

    Cambiando errores

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ class Server {
 
     // Middleware
     this.app.use(cors({
-      origin:  'https://ecommerce-ropa-nodejs-production.up.railway.app', 
+      origin:  'https://capable-cat-98c74c.netlify.app',
       methods: 'GET, POST, PUT, DELETE, OPTIONS',
       allowedHeaders: ['Content-Type', 'Authorization'],
       credentials: true,
